dd549a7c6f726921c81e65168780acdbc235c851
Diego Naranjo <da.naranjo10@uniandes.edu.co>
2025-04-06 16:35:55 -0500

    conexion a back (#19)

---
+++ logistica-ccp/src/app/stock/adapters/microserviceStock.ts
@@ -0,0 +1,32 @@
+const apiURI = process.env.NEXT_PUBLIC_BFF_HOST
+// const apiURI = 'http://127.0.0.1:3006/'
+
+export const updateStock = async (stockData: {
+    id_producto: string;
+    cantidad: number;
+}): Promise<void> => {
+    const url = apiURI + '/api/stock/stock_ingresar_inventario'
+    // const url = apiURI + '/stock_ingresar_inventario'
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(stockData),
+        });
+
+        if(!response.ok) {
+            const error_msg = await response.text()
+            throw new Error("No ha sido posible crear un nuevo stock: " + error_msg)
+        }
+
+        const data = await response.json()
+        console.log("Stock creado con éxito:", data)
+
+    } 
+    catch (error) {
+        console.error("No ha sido posible crear un nuevo stock", error)
+    }
+}
