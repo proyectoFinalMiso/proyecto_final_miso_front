a939e5e6c02c7e9309e2db8ee4ed9fa8a6cb0116
OrangeScript <da.naranjo10@uniandes.edu.co>
2025-04-12 19:31:28 -0500

    tabla productos en bodegas

--- logistica-ccp/src/app/stock/page.tsx
+++ logistica-ccp/src/app/stock/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import styles from "./Stock.module.css"
 import DataTable from "../../../globalComponents/Datatable";
@@ -16,6 +16,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import AddIcon from '@mui/icons-material/Add';
 
+import { getStock } from "./adapters/microserviceStock";
+
+
 declare module '@mui/material/Button' {
     interface ButtonPropsColorOverrides {
         cpp: true;
@@ -23,9 +26,36 @@ declare module '@mui/material/Button' {
     }
 }
 
-const Stock: React.FC = () => {
+interface Stock {
+    nombre: string;
+    bodega: string;
+    posicion: string;
+    sku: string;
+    lote: string;
+    cantidad: number;
+}
 
+const Stock: React.FC = () => {
+    const tableSchema: GridColDef[] = [
+        {field: 'nombre', headerName: 'Nombre Producto', flex: 4, headerClassName: styles.Header},
+        {field: 'bodega', headerName: 'Bodega', flex: 1, headerClassName: styles.Header},
+        {field: 'posicion', headerName: 'Posición', flex: 1, headerClassName: styles.Header},
+        {field: 'sku', headerName: 'SKU', flex: 1, headerClassName: styles.Header},
+        {field: 'lote', headerName: 'Lote', flex: 1, headerClassName: styles.Header},
+        {field: 'cantidad', headerName: 'Cantidad', flex: 1, type: 'number', headerClassName: styles.Header},
+    ]
+
+    const [stock, setStock] = useState<Stock[]>([])
     const [isOpenProducto, setIsOpenProducto] = useState(false);
+
+    const fetchStock = async () => {
+                const stockList = await getStock();
+                setStock(stockList);
+            }
+    
+    useEffect(() => {
+            fetchStock();
+        }, []);    
 
     return (
         <ThemeProvider theme={theme}>
@@ -77,6 +107,9 @@ const Stock: React.FC = () => {
                                     </Button>
                                 </Stack>
                             </Grid>
+                        </Grid>
+                        <Grid size="grow" sx={{ margin: '1.25rem 6.25rem' }}>
+                            <DataTable columns={tableSchema} rows={stock} />
                         </Grid>
                     </Grid>
                 </Grid>
